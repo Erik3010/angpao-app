d2fda0d965bccf7be4515e9e6a2e9f0d08827c3b
Erik <erikhau3010@gmail.com>
2021-10-09 22:14:01 +0700

    deploy

---
+++ precache-manifest.0b9995674ca7b64c2c4da082904f7e41.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ba1a36111fd7af6109",
+    "url": "/angpao-app/css/app.70ae152c.css"
+  },
+  {
+    "revision": "656753569aef606dd528cc6bdf672cdc",
+    "url": "/angpao-app/fonts/Manrope-Bold.ttf"
+  },
+  {
+    "revision": "47e356f61dca7aa2dfba5593e000c4f1",
+    "url": "/angpao-app/fonts/Manrope-ExtraBold.ttf"
+  },
+  {
+    "revision": "a4b068ee8a8bdb4d976648992bb1db90",
+    "url": "/angpao-app/fonts/Manrope-ExtraLight.ttf"
+  },
+  {
+    "revision": "55aaaa1366df7c6544c2204b032a6e31",
+    "url": "/angpao-app/fonts/Manrope-Light.ttf"
+  },
+  {
+    "revision": "6196e0dab83345b15290ee22620358c1",
+    "url": "/angpao-app/fonts/Manrope-Medium.ttf"
+  },
+  {
+    "revision": "0b726174d2b7e161b9e5e8125bf7751a",
+    "url": "/angpao-app/fonts/Manrope-Regular.ttf"
+  },
+  {
+    "revision": "255d425d09667bc095e79a8bd8081aba",
+    "url": "/angpao-app/fonts/Manrope-SemiBold.ttf"
+  },
+  {
+    "revision": "5a259a9917fb509896bce2cfe4bd7287",
+    "url": "/angpao-app/img/angpao.5a259a99.svg"
+  },
+  {
+    "revision": "1ce5908eed7beb664e1ec568c30611f6",
+    "url": "/angpao-app/img/avatar.1ce5908e.png"
+  },
+  {
+    "revision": "60ac2f4d1f63d2e03de1a94afc34a37e",
+    "url": "/angpao-app/img/banner.60ac2f4d.png"
+  },
+  {
+    "revision": "a126c9e7684285d3d4aaecc30315d73e",
+    "url": "/angpao-app/img/line-deco.a126c9e7.png"
+  },
+  {
+    "revision": "667a895f4b59469a6b7a3491d322d095",
+    "url": "/angpao-app/index.html"
+  },
+  {
+    "revision": "98ba1a36111fd7af6109",
+    "url": "/angpao-app/js/app.bd87bec5.js"
+  },
+  {
+    "revision": "6ffdd4de37cbf32ba7c5",
+    "url": "/angpao-app/js/chunk-16a1c062.905752ea.js"
+  },
+  {
+    "revision": "7358a8edcebe001d662a",
+    "url": "/angpao-app/js/chunk-180b52f4.b5878509.js"
+  },
+  {
+    "revision": "07e66243020c5b873138",
+    "url": "/angpao-app/js/chunk-2d0ae5b5.1c9967cd.js"
+  },
+  {
+    "revision": "eb508014b93cb8b891e6",
+    "url": "/angpao-app/js/chunk-2d21a3d2.32adb5af.js"
+  },
+  {
+    "revision": "1248c029a7b1730f8069",
+    "url": "/angpao-app/js/chunk-2d2213dc.f5170867.js"
+  },
+  {
+    "revision": "052645d70ccb5641d41b",
+    "url": "/angpao-app/js/chunk-2d237161.1daeddf6.js"
+  },
+  {
+    "revision": "3e86bd7ac1d0c0fc27eb",
+    "url": "/angpao-app/js/chunk-2d237ca6.3a29f6d0.js"
+  },
+  {
+    "revision": "9b750320c58625ec01ec",
+    "url": "/angpao-app/js/chunk-333eab73.d181d4e1.js"
+  },
+  {
+    "revision": "e91d4485142a3ea48503",
+    "url": "/angpao-app/js/chunk-4bd56d6a.901db774.js"
+  },
+  {
+    "revision": "bc0c5b2a8b077172d846",
+    "url": "/angpao-app/js/chunk-52080140.d3ed06d1.js"
+  },
+  {
+    "revision": "19bad4f12dc23c5177da",
+    "url": "/angpao-app/js/chunk-5a008568.022d4eb6.js"
+  },
+  {
+    "revision": "b1fe36c3903387db7f33",
+    "url": "/angpao-app/js/chunk-b5dcc802.82b574c4.js"
+  },
+  {
+    "revision": "3ae77e100e1a8514f550",
+    "url": "/angpao-app/js/chunk-f7c7f0e4.b132eaaa.js"
+  },
+  {
+    "revision": "f28e1fe54cbb3ca988a4",
+    "url": "/angpao-app/js/chunk-vendors.7e47f066.js"
+  },
+  {
+    "revision": "071718b718a840633a66d4dd29a4e454",
+    "url": "/angpao-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/angpao-app/robots.txt"
+  }
+]);
